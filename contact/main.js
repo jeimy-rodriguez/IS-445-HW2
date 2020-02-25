b774b5637c0ab45391e6cc9fde47c609c190ed0a
Jeimy Rodriguez <jeimyrodriguez@dhcp-39-75-32.dhcpint.csulb.edu>
2020-02-24 18:41:58 -0800

    javascript

---
+++ contact/main.js
@@ -0,0 +1,21 @@
+var inputName = document.getElementById('input-name');
+var inputMail = document.getElementById('input-mail');
+var inputMessage = document.getElementById('input-message');
+var button = document.querySelector('.submit-button');
+var regEx = /\S+@\S+\.\S+/;
+var form = document.querySelector('.form')
+
+function checkInput () {
+    if (inputName.nodeValue.trim()!== "" && regEx.test(inputMail.value) && inputMessage.value.trim() !== "") {
+        button.getElementsByClassName.backgroundColor = '#fa923d';
+        button.disabled = false; 
+    }   else {
+        button.getElementsByClassName.backgroundColor = '#979695';
+        button.disabled = true;
+    }
+}
+
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    console.log('Submitted');
+}) 
